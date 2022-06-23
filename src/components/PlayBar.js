a57a6291892e6d65c4ee6594f1fed82917905361
pauldbd <loldeadmemegod@gmail.com>
2022-06-22 22:18:18 -0600

    color scheme

---
+++ src/components/PlayBar.js
@@ -0,0 +1,15 @@
+import * as React from "react"; 
+import {pageTitle, slider, playBarDiv} from "./PlayBar.module.css"; 
+
+const PlayBar = (props)=>{
+
+    return (
+        <div className={playBarDiv}> 
+            <p className={pageTitle}>{props.title}</p>
+            <div className={slider} style={{width: props.width, transition: "ease-in-out all .1s"}}></div>
+
+        </div>
+    )
+}
+
+export default PlayBar; 
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,12 +1,13 @@
 import * as React from "react"; 
 import {textDiv} from "./Text.module.css"
-import {titleText} from "./Title.module.css"; 
+import PlayBar from "./PlayBar"
 
 const Texts = ({props, children})=>{
 
     const [text, setText] = React.useState(""); 
     const [count, setCount] = React.useState(0); 
     const [time, setTime] = React.useState(0); 
+    const [width, setWidth] = React.useState(0); 
 
     React.useEffect(()=>{
         
@@ -14,16 +15,23 @@ const Texts = ({props, children})=>{
             if (count < children.length && count >= 0){
                 setText(text + children[count]); 
                 console.log(count, children[count]); 
+
             }
             setCount(count + 1); 
+            setWidth(count/children.length * 100); 
         }, Math.random() * 70 + 40)
         return ()=>clearInterval(intervalId); 
     }); 
 
     return (
-        <div className={textDiv}>
-            {text}
+        <div>
+            <div className={textDiv}>
+                {text}
+            </div>
+            <PlayBar title="Main" width={width + "%"}/>
         </div>
+
+
     )
 }
 
