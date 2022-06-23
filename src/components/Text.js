f04f4dbea3d792e2ad356af04170ebba5ca3ecf9
pauldbd <loldeadmemegod@gmail.com>
2022-06-23 06:40:54 -0600

    size fixed, got rid of bar

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,13 +1,11 @@
 import * as React from "react"; 
 import {textDiv} from "./Text.module.css"
-import PlayBar from "./PlayBar"
+// import PlayBar from "./PlayBar"
 
 const Texts = ({props, children})=>{
 
     const [text, setText] = React.useState(""); 
     const [count, setCount] = React.useState(0); 
-    const [time, setTime] = React.useState(0); 
-    const [width, setWidth] = React.useState(0); 
 
     React.useEffect(()=>{
         
@@ -18,7 +16,6 @@ const Texts = ({props, children})=>{
 
             }
             setCount(count + 1); 
-            setWidth(count/children.length * 100); 
         }, Math.random() * 70 + 40)
         return ()=>clearInterval(intervalId); 
     }); 
@@ -28,7 +25,6 @@ const Texts = ({props, children})=>{
             <div className={textDiv}>
                 {text}
             </div>
-            <PlayBar title="Main" width={width + "%"}/>
         </div>
 
 
